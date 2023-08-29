ae14d8100ad8973c1cc5b86241e0172a0f911734
yedidya rashi <Yedidyarashi@gmail.com>
2023-08-29 13:10:00 +0300

    feat: add output flag

--- source/cli.ts
+++ source/cli.ts
@@ -4,7 +4,7 @@ import { getEntitiesTypescriptPostgres } from "./getEntityTypescriptPostgres.js"
 import yargs from "yargs/yargs";
 
 async function main() {
-	const { connectionString } = await yargs(process.argv.slice(2))
+	const { connectionString, output } = await yargs(process.argv.slice(2))
 		.options({
 			connectionString: {
 				type: "string",
@@ -12,12 +12,15 @@ async function main() {
 				description:
 					"Your PostgreSQL database connection string. Only PostgreSQL databases are supported.",
 			},
+			output: {
+				default: "./src/shared",
+			},
 		})
 		.example([
 			["remult --connectionString postgres://user:pass@host:port/db-name"],
 		]).argv;
 
-	await getEntitiesTypescriptPostgres(connectionString);
+	await getEntitiesTypescriptPostgres(connectionString, output);
 }
 
 main().catch(console.error);
