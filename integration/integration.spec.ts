431fe3ad80d2b3a2ab6ebd118a376f9b84413eb4
yedidya rashi <yedidyarshi@gmail.com>
2024-01-05 00:56:01 +0200

    add the test

--- integration/integration.spec.ts
+++ integration/integration.spec.ts
@@ -1,9 +1,19 @@
-import { describe, test } from "vitest"
+import { describe, expect, test } from "vitest"
 import { promisify } from "util"
 import { exec as child_process_exec } from "child_process"
 describe('postgres tests', () => {
-    test('sanity', async () => {
-        const exec = promisify(child_process_exec);
-        await exec("pnpm start pull --output ./output --connectionString postgres://postgres:postgres@localhost:5432")
+    describe("check that file stracture is correct", () => {
+        test('when db is empty shoud return bare file structure', async () => {
+            const exec = promisify(child_process_exec);
+            await exec("pnpm start pull --output ./output --connectionString postgres://postgres:postgres@localhost:5432")
+            const rootLs = await exec("ls output")
+
+            expect(rootLs.stdout).toBe("entities\nenums\n")
+
+            const entitiesLs = await exec("ls ./output/entities")
+            console.log(entitiesLs, 1);
+
+            expect(entitiesLs.stdout).toBe("index.ts\n")
+        })
     })
 });
