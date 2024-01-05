61944fa99c1f698535c4e7c1aeaefa426724e3c5
yedidya rashi <yedidyarshi@gmail.com>
2024-01-05 13:45:00 +0200

    update bookstore schema test

--- integration/integration.spec.ts
+++ integration/integration.spec.ts
@@ -1,24 +1,77 @@
-import { describe, expect, test } from "vitest"
-import { promisify } from "util"
-import { exec as child_process_exec } from "child_process"
-
+import { describe, expect, test } from "vitest";
+import { promisify } from "util";
+import { exec as child_process_exec } from "child_process";
+const genId = () => {
+    let count = 10
+    return () => {
+        count++
+        return `output${count}`
+    }
+}
+const getOutput = genId()
 const exec = promisify(child_process_exec);
+const connctionString = "postgres://postgres:postgres@localhost:5432"
+const lsOutputToArray = (stdout: string) => stdout.slice(0, -1).split("\n")
 
-describe('postgres tests', () => {
+describe("postgres tests", () => {
     describe("check that file stracture is correct", () => {
-        test('when db is empty shoud return bare file structure', async () => {
-            await exec("pnpm start pull --output ./output --connectionString postgres://postgres:postgres@localhost:5432")
-            const rootLs = await exec("ls output")
+        const output = getOutput()
+        test("when db is empty shoud return bare file structure", async () => {
+            await exec(
+                `pnpm start pull --output ./${output} --connectionString ${connctionString}`,
+            );
+            const rootLs = await exec(`ls ${output}`);
 
-            expect(rootLs.stdout).toBe("entities\nenums\n")
+            expect(lsOutputToArray(rootLs.stdout)).toStrictEqual(["entities", "enums"]);
 
-            const entitiesLs = await exec("ls ./output/entities")
-            expect(entitiesLs.stdout).toBe("index.ts\n")
-        })
-    })
+            const entitiesLs = await exec(`ls ./${output}/entities`);
+            expect(lsOutputToArray(entitiesLs.stdout)).toStrictEqual(["index.ts"]);
+        });
+    });
     describe("bookstore schema", () => {
-        test('just genrate for now', async () => {
-            await exec("pnpm start pull --output ./output1 --connectionString postgres://postgres:postgres@localhost:5432/bookstore_db --schemas bookstore")
-        })
-    })
+        test("when pull with schemas flag", async () => {
+            const output = getOutput()
+            await exec(
+                `pnpm start pull --output ./${output} --connectionString ${connctionString}/bookstore_db --schemas bookstore`,
+            );
+            const entitiesLs = await exec(`ls ./${output}/entities`);
+            expect(lsOutputToArray(entitiesLs.stdout)).toStrictEqual([
+                "Bookstore_Author.ts",
+                "Bookstore_Book.ts",
+                "Bookstore_Customer.ts",
+                "Bookstore_Order.ts",
+                "Bookstore_OrderItem.ts",
+                "index.ts",
+            ]);
+            const authorFile = await exec(`cat ./${output}/entities/Bookstore_Author.ts`);
+
+            expect(authorFile.stdout).toStrictEqual(
+                `import { Entity, Fields } from 'remult'
+import { Relations } from 'remult'
+import { Bookstore_Book } from '.'
+
+@Entity<Bookstore_Author>('authors', {
+\tallowApiCrud: true,
+\tdbName: 'bookstore.authors',
+\tdefaultOrderBy: { name: 'asc' },
+\tid: { author_id: true }
+})
+export class Bookstore_Author {
+\t@Fields.autoIncrement()
+\tauthor_id!: number
+
+\t@Fields.string()
+\tname!: string
+
+\t@Fields.string({ allowNull: true })
+\tbio?: string
+
+  // Relations toMany
+\t@Relations.toMany(() => Bookstore_Book)
+\tbooks?: Bookstore_Book[]
+}
+`)
+
+        });
+    });
 });
