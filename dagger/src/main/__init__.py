17f8f4d8a0a22a0bd809ce60198a44a567bd9e94
yedidyar-ni <yedidya.r@next-insurance.com>
2024-05-29 10:29:39 +0300

    fix last test

--- dagger/src/main/__init__.py
+++ dagger/src/main/__init__.py
@@ -1,55 +1,49 @@
-import contextlib
-
 import dagger
 from dagger import dag, function, object_type
-
-
-@contextlib.asynccontextmanager
-async def managed_service(svc: dagger.Service):
-	"""Start and stop a service."""
-	yield await svc.start()
-	await svc.stop()
 
 
 @object_type
 class RemultCli:
 	@function
-	async def test(self, source: dagger.Directory) -> str:
-		"""Return the result of running unit tests"""
-		postgres = (
+	def test_db_srv(self) -> dagger.Service:
+		"""Return a Postgres DB ready for running tests"""
+		return (
 			dag.container()
 			.from_("postgres:14-alpine")
 			.with_env_variable("POSTGRES_PASSWORD", "postgres")
 			.with_env_variable("POSTGRES_USER", "postgres")
 			.with_env_variable("POSTGRES_DB", "bookstore_db")
-			.with_mounted_cache("/var/lib/postgresql/data", dag.cache_volume("my-postgres"))
 			.with_exposed_port(5432)
 			.as_service()
 		)
 
-		async with managed_service(postgres) as postgres_srv:
-			await (
-				dag.container()
-				.from_("postgres:14-alpine")
-				.with_service_binding("db", postgres_srv)
-				.with_directory("/app", source)
-				.with_workdir("/app")
-				.with_exec(
-					["/usr/local/bin/psql", "postgres://postgres:postgres@db:5432", "-a", "-f",
-					 "/app/scripts/db/bookstore-schecma.sql"])
-			)
+	@function
+	async def test(self, source: dagger.Directory) -> str:
+		"""Return the result of running unit tests"""
+		postgres_srv = self.test_db_srv()
+		return await (
+			self.build_env(source)
+			.with_service_binding("db", postgres_srv)
+			.with_exec(["bash", "-c",
+						"psql postgresql://postgres:postgres@db:5432/bookstore_db -a -f scripts/db/bookstore-schecma.sql"])
+			.with_exec(["pnpm", "test:ci"])
+			.stdout()
+		)
 
-			return await (
-				self.build_env(source)
-				.with_service_binding("db", postgres_srv)
-				.with_exec(["pnpm", "test"])
-				.stdout()
-			)
+	@function
+	def debug(self, source: dagger.Directory) -> dagger.Container:
+		"""Return container with db for debugging tests"""
+		postgres_srv = self.test_db_srv()
+		return (
+			self.build_env(source)
+			.with_service_binding("db", postgres_srv)
+		)
 
 	@function
 	def build_env(self, source: dagger.Directory) -> dagger.Container:
 		"""Build a ready-to-use development environment"""
 		node_cache = dag.cache_volume("node")
+		dist_cache = dag.cache_volume("dist")
 		return (
 			dag.container()
 			.from_("node:21-slim")
@@ -58,4 +52,8 @@ class RemultCli:
 			.with_workdir("/src")
 			.with_exec(["npm", "install", "-g", "pnpm"])
 			.with_exec(["pnpm", "install"])
+			.with_mounted_cache("/src/dist", dist_cache)
+			.with_exec(["pnpm", "build"])
+			.with_exec(["apt", "update"])
+			.with_exec(["apt", "install", "postgresql-client", "-y"])
 		)
